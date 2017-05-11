4c38a61ee694ca5895d63ef0a8f334c8abfe3be3
7702063479 <manju.mca2015@gmail.com>
2017-05-11 09:00:19 +0530

    Added customer related stuff in DAO layer

---
+++ src/com/hms/dao/imp/CustomerDaoImpl.java
@@ -0,0 +1,40 @@
+package com.hms.dao.imp;
+
+import java.util.List;
+
+import com.hms.dao.service.CustomerDao;
+import com.hms.model.Customer;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+	@Override
+	public int insertCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteCustomer(int customerId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Customer getCustomer(int customerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/hms/dao/service/CustomerDao.java
@@ -0,0 +1,18 @@
+package com.hms.dao.service;
+
+import java.util.List;
+
+import com.hms.model.Customer;
+
+public interface CustomerDao {
+	
+	/**
+	 * CRUD operations for CUSTOMER table
+	 */
+	int insertCustomer(Customer customer);
+	int updateCustomer(Customer customer);
+	int deleteCustomer(int customerId);
+
+	Customer getCustomer(int customerId);
+	List<Customer> getCustomers();
+}
