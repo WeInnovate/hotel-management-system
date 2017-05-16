8007193c00f5a8d3bb8389518552f6e1c031e481
7702063479 <manju.mca2015@gmail.com>
2017-05-16 20:57:24 +0530

    added impl of EmployeeDao and EmployeeDao.java

--- src/com/hms/dao/service/EmployeeDao.java
+++ src/com/hms/dao/service/EmployeeDao.java
@@ -1,5 +1,17 @@
 package com.hms.dao.service;
 
+import java.util.List;
+
+import com.hms.model.Employee;
+
 public interface EmployeeDao {
+	
+	
+	int insertEmployee(Employee employee);
+	int updateEmployee(Employee employee);
+	int deleteEmployee(int employeeId);
+
+	Employee getEmployee(int employeeId);
+	List<Employee> getEmployees();
 
 }
